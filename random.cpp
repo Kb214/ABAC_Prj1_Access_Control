a9e72b82419013c0729832d22f1a26a28c69ccd4
Karleigh Branscomb <kbranscomb@ualr.edu>
2026-09-23 01:33:45 +0000

    random works with numbers

--- random.cpp
+++ random.cpp
@@ -5,21 +5,50 @@
 #include <algorithm>
 #include <unordered_set>
 
+struct User_Attrib{
+    int u;
+    std::string att;
+};
+
+
 int main(){
+    // std::vector<std::string> attr{"student","staff","execute","read","write"};
+    // std::vector<std::string> usr_attr;
+
+    // std::random_device rd_attr;
+    // // std::uniform_int_distribution<size_t> dist(2, attr.size()- 1);
+    // // std::shuffle(attr.begin(), attr.end(),rd_attr);
+    // usr_attr.resize(2, attr.size());
+
+    // std::mt19937 gen(rd_attr());
+    // std::ranges::sample(attr);
+    // std::unordered_set<std::string> attr_set;
+
+    int users = 10;
+    int n = 5;
+    
     std::vector<std::string> attr{"student","staff","execute","read","write"};
-    std::vector<std::string> usr_attr;
+    std::vector<User_Attrib> UA;
 
     std::random_device rd_attr;
-    // std::uniform_int_distribution<size_t> dist(2, attr.size()- 1);
-    // std::shuffle(attr.begin(), attr.end(),rd_attr);
-    usr_attr.resize(2, attr.size());
+    std::mt19937 gen(rd_attr());
 
-     std::mt19937 gen(rd_attr());
-     std::ranges::sample(attr,)
-    std::unordered_set<std::string> attr_set;
-    for(auto a : attr){
-        std::cout << a << " " << '\n';
+
+    for(int i = 0; i < users; i++){
+
+        std::shuffle(attr.begin(), attr.end(), gen);
+
+        std::uniform_int_distribution<size_t> dist(2,n);
+        int random = dist(gen);
+
+
+        std::cout << "User " << i + 1 << ": " << random;
+        std::cout << "\n\n";
     }
+
+    // for(auto a : attr){
+    // std::cout << a << " " << '\n';
+    // }
 
 
     return 0;
---
+++ secondtry.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <list>
+#include <string>
+#include <cstdlib>
+#include <random>
+#include <ctime>
+
+struct User_Attrib{
+    int User_ID;
+    std::string random_attrib; 
+};
+
+int main(){
+
+    int att;
+    std::vector<User_Attrib> UA;
+    std::vector<a
+
+    for(int i=0; i < att; att++){
+        std::cin >> UA;
+        UA.attrib.push_back();
+    }
+
+
+    return 0;
+}
