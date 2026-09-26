a896220267d6076d411eb394959a6cc444e4fb93
Karleigh Branscomb <kbranscomb@ualr.edu>
2026-09-26 02:32:04 +0000

    figured out predicate inputs; still some bugs to fix

--- secondtry.cpp
+++ secondtry.cpp
@@ -27,8 +27,47 @@ DESCRIPTION:
 struct User_Attrib{
     int User_ID;
 
-    std::string random_attrib; 
+    std::vector<std::string> random_attrib; 
 };
+
+std::vector<User_Attrib> UA;
+
+bool attribSearch(int userID, std::string& attr){
+    for(auto& u: UA){
+        if(u.User_ID == userID){
+            for(auto& a: u.random_attrib){
+                if(a == attr){
+                    return true;
+                    std::cout<< "Policy is valid.\n";
+                }
+            }
+        }
+    }
+    return false;
+    std::cout<< "Policy is invalid.\n";
+}
+
+bool AND(std::string& attr1, std::string& attr2, int userID){
+
+
+    if(attribSearch(userID, attr1) && attribSearch(userID, attr2)){
+        return true;
+    } else{
+        return false;
+    }
+
+}
+
+bool OR(std::string& attr1, std::string& attr2, int userID){
+
+    if(attribSearch(userID, attr1) || attribSearch(userID, attr2)){
+        return true;
+    } else{
+        return false;
+    }
+
+}
+
 
 int main(){
     int numb_att;
@@ -37,9 +76,9 @@ int main(){
 
     std::cout << "How many attributes? : ";
     std::cin >> numb_att;
-
-    std::vector<User_Attrib> UA;
     
+    
+
 
     for(int i = 0; i < numb_att; i++){
         std::cout << "Enter Attribute: ";
@@ -53,32 +92,32 @@ int main(){
     std::mt19937 gen(rd());
 
     std::cout << "Generated Users and Attributes:\n";
+
     for(int p = 0; p < 10; p++){
-        
         std::shuffle(a.begin(),a.end(), gen);
         std::uniform_int_distribution<size_t> dist(2,numb_att);
         std::string rand_a = a[dist(gen)];
         int random = dist(gen);
 
-    
+
         std::cout << "User " << p + 1 << ": {";
 
         for(int i = 0; i < random; i++){
             std::cout <<  a.at(i) << ", ";
-            UA.emplace_back(User_Attrib{p + 1,a.at(i)});
+            UA.emplace_back(User_Attrib{p + 1,{a.at(i)}});
             
         }
-    
+
         std::cout << "}";    
 
         std::cout << "\n";
-
     }
 
     
     while(true){
         std::cout << "\nEnter UserID: ";
         int id;
+
         std::cin >> id;
 
         std::cout << "\nEnter Attribute-Based Policy for the user: ";
@@ -87,32 +126,65 @@ int main(){
         std::getline(std::cin, policy);
 
 
-        if(policy == UA[id - 1].random_attrib){
+        if(attribSearch(id, policy)){
             std::cout << "Policy is valid.\n";
-            std::cout << "User " << id << " has the attribute " << policy << ".\n";
 
-        } else{
-            std::cout << "Policy is invalid. Please enter a valid policy.\n";}
+        } else if(policy.find("AND")){
 
-        // if(policy == "AND" || policy == "OR" && policy == attribute || policy == attribute){
-        //     std::cout << "Policy is valid.\n";
-        //     std::cout << "User " << id << " has the attribute " << attribute << ".\n";
-        // } else if(policy != "AND" && policy != "OR" && policy != attribute || policy != attribute){
-        //     std::cout << "Policy is invalid. Please enter a valid policy.\n";
-        //     std::cout << "User " << id << " does not have the attribute " << attribute << ".\n";
-        // } else{
-        //     std::cout << "Invalid input. Please enter a valid policy or attribute.\n";
-        // }
+            AND(a[0], a[1], id);
+
+
+            if(AND(a[0],a[1], id) == true){
+                std::cout << "policy is valid.\n";
+            } else{
+                std::cout << "policy is invalid.\n";
+            }
+
+
+        } else if(policy.find("OR")){
+
+            OR(a[0], a[1], id);
+
+
+            if(OR(a[0], a[1], id) == true){
+                std::cout << "policy is valid.\n";
+            } else{
+                std::cout << "policy is invalid.\n";
+            }
+
+
+        } else if(policy.find("AND") && policy.find("OR")){
+
+            AND(a[0], a[1], id) || OR(a[0], a[1], id);
+
+
+            if(AND(a[0], a[1], id) || OR(a[0], a[1], id)==true){
+                std::cout << "policy is valid.\n";
+            } else{
+                std::cout << "policy is invalid.\n";
+            }
+
+
+        }else{
+
+            std::cout << "Policy is invalid. Please enter a valid policy.\n";
+
+        }
+
+        return false;
     }
 
 
-    // std::cout << "\nall user attributes stored in struct and vector: \n";
-    // for(auto m : UA){
+    std::cout << "\nall user attributes stored in struct and vector: \n";
 
-    //     std::cout << m.User_ID << ": " << m.random_attrib << "\n\n";
-    // }
+    for(auto m : UA){
 
-   
-
+        std::cout << m.User_ID << ": ";
+        for(const auto& attr : m.random_attrib){
+            std::cout << attr << " ";
+        }
+        std::cout << "\n\n";
+    }
     return 0;
+
 }
